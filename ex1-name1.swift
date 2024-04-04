f40d3354c3d1c25ea6eb2df254becc23bafcb559
TonyAtTeky <6301468-TonyAtTeky@users.noreply.replit.com>
2024-04-04 08:02:43 +0000

    Initial commit

---
+++ ex1-name1.swift
@@ -0,0 +1,18 @@
+/*
+Project: Grade Evaluator
+Description:
+Build a command line application that allows the user to enter a student's grade and displays the corresponding grade based on that score.
+
+Requirements:
+Input the grade from the user (e.g., from 0 to 100).
+
+Use the if statement to evaluate the grade and determine the corresponding grade.
+
+Print out the student's grade based on the score:
+
+Score from 90 and above: Excellent
+Score from 80 to below 90: Good
+Score from 70 to below 80: Fair
+Score from 60 to below 70: Average
+Score below 60: Poor
+*/
---
+++ ex2-name2.swift
@@ -0,0 +1,10 @@
+/*
+Project: Odd-Even Number Checker
+Description:
+Build a command line application to check whether an integer entered by the user is an odd number or an even number.
+
+Requirements:
+Input an integer from the user.
+Use the if statement to check if the number is divisible by 2 or not.
+Print out the result: "X is an even number" or "X is an odd number".
+*/
